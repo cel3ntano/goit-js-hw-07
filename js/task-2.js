4e60c412dcbe9d780d191340878564320d1d8709
cel3ntato <adriano2024celentano@gmail.com>
2024-04-26 05:09:44 +0200

    hw7 task-2

--- js/task-2.js
+++ js/task-2.js
@@ -22,5 +22,17 @@ const images = [
   {
     url: "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
     alt: "Lighthouse Coast Sea",
-  }
+  },
 ];
+
+const gallery = document.querySelector(".gallery");
+gallery.style.display = "flex";
+gallery.style.flexWrap = "wrap";
+const galleryItems = images
+  .map(
+    (image) =>
+      `<li><img src="${image.url}" alt="${image.alt}" width="300"></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryItems);
